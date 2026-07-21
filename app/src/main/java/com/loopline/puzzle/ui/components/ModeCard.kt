765b48cab85ece3aadd5c4e636832608f41be568
Zain Khan Swati <zainkhanswati54@gmail.com>
2026-07-21 11:41:47 +0500

    yes

--- app/src/main/java/com/loopline/puzzle/ui/components/ModeCard.kt
+++ app/src/main/java/com/loopline/puzzle/ui/components/ModeCard.kt
@@ -32,10 +32,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.graphicsLayer
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
